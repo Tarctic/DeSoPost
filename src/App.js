bff9d17647203b821c9848ffa45fb5ab1ff8e24b
Tarctic <zainabshah2003@gmail.com>
2023-01-11 18:07:54 +0300

    Add files

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import "./App.css";
+import Deso from "deso-protocol";
+import { useState } from "react";
+const deso = new Deso();
+console.log(deso)
+function App() {
+  const [sampleResponse, setSampleResponse] = useState();
+  const [loginResponse, setLoginResponse] = useState();
+  const [postResponse, setPostResponse] = useState();
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          const user = await deso.identity.login();
+          console.log(user);
+          setLoginResponse(JSON.stringify(user, null, 2));
+        }}
+      >
+        login
+      </button>
+      <button
+        onClick={() => {
+          deso.identity.logout(deso.identity.getUserKey());
+        }}
+      >
+        logout
+      </button>
+      <button
+        onClick={async () => {
+          const user = await deso.user.getSingleProfile({
+            PublicKeyBase58Check: deso.identity.getUserKey(),
+          });
+          console.log(user);
+          setSampleResponse(JSON.stringify(user, null, 2));
+        }}
+      >
+        get user
+      </button>
+      <button onClick={async() => {
+           const request = {
+             "UpdaterPublicKeyBase58Check": deso.identity.getUserKey(),
+             "BodyObj": {
+               "Body": "Checking out the developer hub",
+               "VideoURLs": [],
+               "ImageURLs": []
+             }
+           };
+           const response = await deso.posts.submitPost(request);
+         }  
+       }> submit post
+       </button>
+      <div>
+        Login info
+        <pre>{loginResponse}</pre>
+      </div>
+      <div>
+        User info
+        <pre>{sampleResponse}</pre>
+      </div>
+      setPostResponse
+      <div>
+        User info
+        <pre>{postResponse}</pre>
+      </div>
+    </div>
+  );
+}
+
+export default App;
